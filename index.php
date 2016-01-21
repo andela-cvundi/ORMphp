5ae48a82e7a4702dbe55b535fe4e6d06ad726e65
andela-cvundi <christophervundi@gmail.com>
2016-01-21 12:54:56 +0300

    (fix) Add error reporting when connecting to the databse

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
+//remove before flight
+ini_set('display_errors', 'On');
+
+try {
+    $db = new PDO('sqlite:./database.db');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo $e->getMessage();
+    die();
+}
+
+
 
 ?>
 
