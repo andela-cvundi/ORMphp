4bdc054fbd8a2c3c74bf9fa7ddc9f73562885927
andela-cvundi <christophervundi@gmail.com>
2016-01-21 12:30:20 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP data Objects</title>
+</head>
+<body id="home">
+    <h1>
+        Sakila sample database
+    </h1>
+    <h2>Films by title</h2>
+    <ul>
+        <li>Film One</li>
+        <li>Film Two</li>
+        <li>Film Three</li>
+        <li>Film Four</li>
+    </ul>
+</body>
+</html>
